0b6abcfe31871c15e7a197d52253d1852856d65c
mitchelloharawild <mitchell@nectric.com.au>
2024-10-02 00:33:12 +1000

    Improve tidyness of staff dataset

--- exercises/staff.R
+++ exercises/staff.R
@@ -3,7 +3,10 @@ library(dplyr)
 library(tsibble)
 
 staff <- read_excel("data-raw/Table 50a In-school Staff (Number), 2006-2023.xlsx", sheet = 3, skip = 4) |> 
-  mutate(across(where(is.character), ~ substring(., 3))) |> 
+  mutate(
+    across(where(is.character), ~ substring(., 3)),
+    Function = stringr::str_to_sentence(Function)
+  ) |> 
   as_tsibble(key = where(is.character), index = Year)
 
 readr::write_rds(staff, "data/staff.rds")
